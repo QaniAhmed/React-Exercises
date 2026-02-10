ef62e24efbf43c89aee733f1aa693c6f224c35c5
QaniAhmed <almualim0@gmail.com>
2026-02-10 12:11:10 +0300

    Add winning function

--- TicTacToe-ui/src/App.jsx
+++ TicTacToe-ui/src/App.jsx
@@ -5,8 +5,12 @@ export default function App(){
   const [Turn,setTurn] = useState('X')
   const [state,setState] = useState(Array(9))
   const [counter,setCounter]= useState(0)
-  const Title = "Player"+ (Turn=='X'?'X':'O') + "s Turn" ; 
+  const TurnTitle = "Player"+ (Turn=='X'?'X':'O') + "s Turn" ; 
   const[GG,setGG] = useState(false) ;
+  //wining array
+  const WinningArray = [[0,1,2],[3,4,5],[6,7,8], //row
+                        [0,3,6],[1,4,7],[2,5,8], //col
+                         [0,4,8],[2,4,6]]; //diagonal 
   //create box array 
 const Boxes = [...Array(9)].map((_, index) => index + 1);
 
@@ -14,16 +18,26 @@ const Boxes = [...Array(9)].map((_, index) => index + 1);
   {
     //take a copy 
   let newBoard = [...state];
-
   //avoid duplicate (if null use it or avoid)
   if(newBoard[index]==null)
     {
       if(GG)return
-      console.log("init counter:"+counter);
+      // console.log("init counter:"+counter);
       newBoard[index]=Turn; //write the chararacter X|O
+
+      //check if there's winner here for current turn
+      WinningArray.map((WinningCase)=>{ //0,1,2
+        if(newBoard[WinningCase[0]]==newBoard[WinningCase[1]]&&newBoard[WinningCase[0]]==newBoard[WinningCase[2]] && newBoard[WinningCase[0]]!=null )
+          {
+            console.log(Turn+" Winner")
+            setGG(true);
+            return
+        }
+      })
+      console.log(newBoard)
       Turn=='X'? setTurn('O') : setTurn('X') //change the turn after write
       setCounter(counter+1)
-      console.log("last counter:"+counter);
+      // console.log("last counter:"+counter);
 
       //update the board
       setState(newBoard)
@@ -50,7 +64,7 @@ const Boxes = [...Array(9)].map((_, index) => index + 1);
   <h1 className="title">Tic-Tac-Toe</h1>
   
   <div className="status-message" style={{fontSize:GG?'4.5rem':'1.5rem'}}>
-    {GG?"Game over":Title}
+    {GG?"Game over":TurnTitle}
   </div>
 
   <div className="board">
